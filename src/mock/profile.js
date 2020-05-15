01c3ccce1dc6c47a7385d16ad71d413a0a38f356
liu2010 <liu2010@qq.com>
2020-05-15 10:10:53 +0800

    init

---
+++ src/mock/profile.js
@@ -0,0 +1,13 @@
+export default {
+    profile:{
+        "code" : "10000",
+        "success" : true,
+        "message" : "登录成功",
+        "data" : {
+            mobile:"138000138000",
+            username:"张三ok",
+            company:"有限公司12345678",
+            email:"cc@ccc.cn",
+        }
+    }
+}
---
+++ src/module-demo/router/index.js
@@ -0,0 +1,26 @@
+ 
+
+import Layout from '@/module-dashboard/pages/layout'
+const _import = require('@/router/import_' + process.env.NODE_ENV)
+
+export default [
+  {
+    root: true,
+    path: '/saas-demo',
+    component: Layout,
+    redirect: 'noredirect',
+    name: 'demo',
+    meta: {
+      title: 'xxx业务模块管理',
+      icon: 'component'
+    },
+    children: [
+      {
+        path: 'index',  //请求地址
+        component: _import('demo/pages/index'), //跳转的vue视图
+        name: 'demo-index',
+        meta: {title: 'demo', icon: 'component', noCache: true}
+      }
+    ]
+  }
+]
